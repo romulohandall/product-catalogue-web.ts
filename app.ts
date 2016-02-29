fd64d9f925e73b24eec093d45456a177b926ded6
Frederik Lölhöffel <frederik@Frederiks-MacBook-Pro-2.local>
2016-02-29 17:17:21 +0100

    Some linting.

--- app.ts
+++ app.ts
@@ -3,10 +3,9 @@ import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {enableProdMode} from 'angular2/core';
 
-import {ContentfulService} from './app/services/contentful-service'
+import {ContentfulService} from './app/services/contentful-service';
 import {ProuductCatalogueApp} from './app/prouduct-catalogue-app';
 
 enableProdMode();
 
-bootstrap(ProuductCatalogueApp, [HTTP_PROVIDERS, ROUTER_PROVIDERS, ContentfulService])
-  .catch(err => console.error(err));
+bootstrap(ProuductCatalogueApp, [HTTP_PROVIDERS, ROUTER_PROVIDERS, ContentfulService]).catch(err => console.error(err));

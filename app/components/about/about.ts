e8dce99e1539e68a77f114d1633ae868140dd484
Frederik Lölhöffel <floelhoeffel@gmail.com>
2016-02-29 11:36:07 +0100

    Initial commit.

---
+++ app/components/about/about.ts
@@ -0,0 +1,19 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'categories',
+  templateUrl: 'app/components/about/about.html',
+  styleUrls: ['app/components/about/about.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+
+export class About {
+
+  constructor() {
+
+
+  }
+}

84579eddd9cf4e6a100400c29763b7d2b3e73d6f
Frederik Lölhöffel <floelhoeffel@gmail.com>
2016-03-04 11:18:23 +0100

    Changed html5 url style to hash style.

--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_PROVIDERS} from 'angular2/http';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-import {enableProdMode} from 'angular2/core';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {enableProdMode, provide} from 'angular2/core';
 
 import {ContentfulService} from './app/services/contentful-service';
 import {ProuductCatalogueApp} from './app/prouduct-catalogue-app';
 
 enableProdMode();
 
-bootstrap(ProuductCatalogueApp, [HTTP_PROVIDERS, ROUTER_PROVIDERS, ContentfulService]).catch(err => console.error(err));
+bootstrap(ProuductCatalogueApp,
+    [HTTP_PROVIDERS, ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy}), ContentfulService,])
+    .catch(err => console.error(err));
